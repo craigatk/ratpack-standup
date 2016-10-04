9d8229c8cde7e54f6dd9f276c34814899bae3a54
catkinson <catkinson@bloomhealthco.com>
2016-10-04 12:19:11 -0500

    Beefing up List Vue unit test

--- src/app/test/unit/specs/List.spec.js
+++ src/app/test/unit/specs/List.spec.js
@@ -7,14 +7,19 @@ const fetchMock = require('fetch-mock')
 
 describe('List.vue', () => {
   it('should display status list', (done) => {
+    let status = {name: 'Name1', yesterday: 'Yesterday1', today: 'Today1', impediments: 'Impediments1'}
+
     fetchMock.get('/api/status/all', () => {
-      return [{name: 'Name1', yesterday: 'Yesterday1', today: 'Today1', impediments: 'Impediments1'}]
+      return [status]
     })
 
     const vm = new Vue(List).$mount()
 
     setTimeout(() => {
-      expect($(vm.$el).find('.panel-heading').text()).toEqual('Name1')
+      expect($(vm.$el).find('.name').text()).toEqual(status.name)
+      expect($(vm.$el).find('.yesterday').text()).toEqual(status.yesterday)
+      expect($(vm.$el).find('.today').text()).toEqual(status.today)
+      expect($(vm.$el).find('.impediments').text()).toEqual(status.impediments)
 
       done()
     })
