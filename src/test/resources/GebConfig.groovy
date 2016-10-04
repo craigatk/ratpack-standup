7f057bb0fa335a426cf18f2dd3cdc29f8dc534ea
catkinson <catkinson@bloomhealthco.com>
2016-10-04 08:42:47 -0500

    Adding support for running browser functional tests in Chrome

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,1 +1,19 @@
+import io.github.bonigarcia.wdm.ChromeDriverManager
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+
 reportsDir = "build/geb"
+
+driver = { new FirefoxDriver() }
+
+environments {
+
+    // run with "gradlew -Dgeb.env=chrome testBrowser"
+    // ChromeDriver reference: https://sites.google.com/a/chromium.org/chromedriver/
+    chrome {
+        // Download and configure ChromeDriver using https://github.com/bonigarcia/webdrivermanager
+        ChromeDriverManager.getInstance().setup()
+
+        driver = { new ChromeDriver() }
+    }
+}
