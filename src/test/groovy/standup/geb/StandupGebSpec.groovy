bcf648f8c6c19b6e436200d3036d2ce06c841df4
Craig <atk.craig@gmail.com>
2016-10-15 14:37:27 -0500

    Adding browser functional test to verify page updates via WebSocket when new status submitted via API

--- src/test/groovy/standup/geb/StandupGebSpec.groovy
+++ src/test/groovy/standup/geb/StandupGebSpec.groovy
@@ -4,6 +4,8 @@ import geb.spock.GebReportingSpec
 import ratpack.groovy.test.GroovyRatpackMainApplicationUnderTest
 import spock.lang.AutoCleanup
 
+import static groovy.json.JsonOutput.toJson
+
 class StandupGebSpec extends GebReportingSpec {
     @AutoCleanup
     def aut = new GroovyRatpackMainApplicationUnderTest()
@@ -31,5 +33,25 @@ class StandupGebSpec extends GebReportingSpec {
         StatusDisplayModule myStatus = homePage.findStatusFor(name)
         assert myStatus?.yesterday == yesterday
         assert myStatus.today == today
+    }
+
+    void "when new status added should update existing users via WebSocket"() {
+        when:
+        HomePage homePage = to HomePage
+
+        then:
+        waitFor { homePage.numberOfStatusDisplays == 0 }
+
+        when: 'adding a new status using the JSON REST API'
+        aut.httpClient.requestSpec { spec ->
+            spec.body { b ->
+                b.text(toJson([name: 'WebSocket Test']))
+            }
+        }.post("api/status")
+
+        then:
+        waitFor { homePage.numberOfStatusDisplays == 1 }
+
+        assert homePage.findStatusFor('WebSocket Test')
     }
 }
